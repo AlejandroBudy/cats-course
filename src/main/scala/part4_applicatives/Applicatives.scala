98d08fb0f226a453bd3a36d79e8115186efac78f
AlejandroBudy <atorresato@gmail.com>
2022-03-20 12:01:38 +0100

    applicatives

---
+++ src/main/scala/part4_applicatives/Applicatives.scala
@@ -0,0 +1,35 @@
+package part4_applicatives
+
+object Applicatives {
+
+  // Applicatives = Functors + the pure method
+
+  import cats.Applicative
+  import cats.instances.list._
+
+  val listApplicative: Applicative[List] = Applicative[List]
+  val aList: List[Int] = listApplicative.pure(2)
+
+  import cats.instances.option._ // implicit Applicative[Option]
+
+  val optionApplicative: Applicative[Option] = Applicative[Option]
+  val anOption: Option[Int] = optionApplicative.pure(2)
+
+  // pure extension method
+
+  import cats.syntax.applicative._
+
+  val aSweetList: List[Int] = 2.pure[List] // List(2)
+  val aSweetOption: Option[Int] = 2.pure[Option]
+
+  // Monads extends Applicatives
+  // Applicatives extends Functors
+
+  def productWithApplicatives[W[_], A, B](wa: W[A], wb: W[B])(implicit applicative: Applicative[W]): W[(A, B)] =
+    applicative.tuple2(wa, wb)
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
