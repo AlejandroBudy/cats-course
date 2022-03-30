c8f72e3c95dba296f0c0e18e8907aae357ca7f80
AlejandroBudy <atorresato@gmail.com>
2022-03-30 16:36:02 +0200

    IO effects

--- src/main/scala/effects/part2_effects/IOIntroduction.scala
+++ src/main/scala/effects/part2_effects/IOIntroduction.scala
@@ -36,6 +36,49 @@ object IOIntroduction {
   def smallProgram(): IO[Unit] =
     (IO(StdIn.readLine()), IO(StdIn.readLine())).mapN(_ + _).map(println)
 
+  /**
+   * Exercises
+   */
+  // Sequence two IO and take the result of the last
+  def sequenceTakeLast[A, B](ioa: IO[A], iob: IO[B]): IO[B] = ioa.flatMap(_ => iob)
+
+  def sequenceTakeLastShort[A, B](ioa: IO[A], iob: IO[B]): IO[B] = ioa *> iob
+
+  // Evaluation is lazy
+  def sequenceTakeLastShort_V2[A, B](ioa: IO[A], iob: IO[B]): IO[B] = ioa >> iob
+
+  // Sequence two IO and take the result of the first
+  def sequenceTakeFirst[A, B](ioa: IO[A], iob: IO[B]): IO[A] = for {
+    a <- ioa
+    _ <- iob
+  } yield a
+
+  def sequenceTakeLastShort[A, B](ioa: IO[A], iob: IO[B]): IO[A] = ioa <* iob
+
+  def forever[A](ioa: IO[A]): IO[A] = ioa.flatMap(_ => forever(ioa))
+
+  def foreverV2[A](ioa: IO[A]): IO[A] = ioa.foreverM
+
+  def convert[A, B](ioa: IO[A], value: B): IO[B] = ioa.map(_ => value)
+
+  def asUnit[A](ioa: IO[A]): IO[Unit] = ioa.as(())
+
+  def asUnitv2[A](ioa: IO[A]): IO[Unit] = ioa.void
+
+  // This stackoverflow
+  def sumIO(n: Int): IO[Int] =
+    if (n <= 0) IO.pure(0)
+    else sumIO(n - 1).map(_ + n)
+
+  // Based on flatMap => lazy evaluation = not crashing
+  def sumIOSafe(n: Int): IO[Int] =
+    if (n <= 0) IO.pure(0)
+    else for {
+      lastNumber <- IO(n)
+      prevNum <- sumIOSafe(n - 1)
+    } yield lastNumber + prevNum
+
+
   def main(args: Array[String]): Unit = {
 
     import cats.effect.unsafe.implicits.global
